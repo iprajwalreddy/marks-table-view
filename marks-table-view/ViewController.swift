4abc3fa63b5534d868e24decdb0287cbea663e5e
P Reddy <prajwalreddy@Ps-iMac.local>
2017-03-16 00:05:06 +0530

    Initial Commit

---
+++ marks-table-view/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  marks-table-view
+//
+//  Created by Prajwal Reddy on 16/03/17.
+//  Copyright © 2017 Prajwal Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
